ef23e502fe868e9d2c3b3543277978075d4e1fd4
Marissa Zhong <mwz2101@columbia.edu>
2018-07-01 16:16:35 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -4,7 +4,7 @@ class MusicImporter
     @file_path = file_path
   end
   def files
-    @files = Dir.glob("#{file_path}/*.mp3").collect{ |f| f.gsub("#{file_path}/", "") }
+    @files ||= Dir.glob("#{file_path}/*.mp3").collect{ |f| f.gsub("#{file_path}/", "") }
   end
   def import
     files.each {|file| Song.create_file_filename(file)}
