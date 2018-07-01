ec8a6bd0104a0fddf65a9ff3bc919d6921faaab2
Marissa Zhong <mwz2101@columbia.edu>
2018-07-01 14:17:06 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save
+    self.all << self
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    new_artist = Artist.new(name)
+    new_artist.save
+  end
+
+end
