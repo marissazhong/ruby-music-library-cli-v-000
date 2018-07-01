84e9eba3a76b1fed44bb60a63ee8134ef2911421
Marissa Zhong <mwz2101@columbia.edu>
2018-07-01 19:01:20 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,6 +19,8 @@ class MusicLibraryController
         list_artists
       elsif input == 'list genres'
         list_genres
+      elsif input == 'list artist'
+        list_artist
       end
     end
   end
